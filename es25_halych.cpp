19815986ecdcbb822d078b662f0abeb1d28371e2
halychmykola-prog <halych.mykola.studente@itispaleocapa.it>
2026-02-03 08:59:54 +0100

    Add files via upload

--- es25_halych.cpp
+++ es25_halych.cpp
@@ -15,25 +15,57 @@ using namespace std;
 
 int main()
 {
+    
+    /*
 	srand(time(0));
 	int row, column;
 	
-	row=rand()%12+4;
-	column=rand()%12+4;
+	row=rand()%4+4;
+	column=rand()%4+4;
 	int vector[row][column];
 	
+	
     for(int i=0;i<row;i++)
-    	for(int j=0;i<column;i++)
+    	for(int j=0;j<column;i++)
     	{
-    		vector[i][j]=rand()%4+4;
+    		vector[i][j]=1;                   //rand()%4+4;
     		cout<<"Vector["<<i<<"]["<<j<<"]="<<vector[i][j]<<endl;
     		
 		}
+	*/
+		
+	
     
-    for(int i=0;i<row;i++)
+    /*for(int i=0;i<row;i++)
     {
-    	cout<<vector[row];
+        for(int j=0;i<column;j++)
+    	cout<<vector[i];
+	}*/
+    
+    
+    
+    
+    //generazione casuale
+    /*
+    if(row*column)%2!=0)
+    {
+    	int rowd = rand()%((sizeof(vector[])/sizeof(vector[0)-1);
+    	int columnd = rand()%((sizeof(vector[])/sizeorowdf(vector[0)-1);
+    	vector[rowd][columnd]=0;
+    	
+    	cout<<"PROVERKAAAAAAA VECTOR CON ZERO RAVNO VECTOR["<<rowd<<"]["<<columnd
 	}
+    
+    
+    
+    
+    */
+    
+    
+    
+
+    
+    
     
     
     
